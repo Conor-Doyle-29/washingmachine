b4c40c362bd39961f0aea098a4c0bf560eda887d
Conor Doyle <18cldoyle@stbenilduscollege.com>
2023-11-20 14:30:00 +0000

    Created Code.py

---
+++ code.py
@@ -0,0 +1,20 @@
+ser.baudrate = 115200
+ser.port = 'COM19'
+ser.open()
+while True:
+    data = str(ser.readline())
+    data= data.replace("b","")
+    data= data.replace("'","")
+    data= data.replace(" ","")
+    data= data.replace("\\r\\n","")
+    if data > str(3180):
+        print("WARNING - READING IS TOO HIGH")
+    print(data)
+
+"""
+    if len(data) > 0:
+        print(data)
+        file = open("accelerometer.csv",'a')
+        file.write(data+",")
+        file.close()
+"""     
